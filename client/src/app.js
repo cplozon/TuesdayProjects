d527291ac0538906e6a6f175df076a54c1a2b6f9
cplozon <cory.lozon@yahoo.com>
2017-10-28 11:49:15 -0400

    newapproute

--- client/src/app.js
+++ client/src/app.js
@@ -3,21 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Diagnostic from './components/ODM';
 import  YouTubeSeach  from './components/YouTube';
 import  Elements  from './components/theme/Elements';
-
-
-
+import AddCarData from './components/addCarData';
 
 const App = (props) => 
-
  {
 		return (
 			<Router>
 			<Switch>
 				<Route exact path={"/"} component={Elements} />
-				
-
 				<Route exact path={"/VideoSearch"} component={YouTubeSeach} />
 				<Route exact path={"/Diagnostic"} component={Diagnostic} />
+				<Route exact path={"/AddYourCar"} component={AddCarData} />
 				</Switch>
 			</Router>
 			);
@@ -28,7 +24,6 @@ const App = (props) =>
     //     <Diagnostic />
         
     // </div>
-
 
 export default App;
 
