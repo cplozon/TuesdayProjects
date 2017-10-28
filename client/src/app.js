581857762823037aa0eb6a6b8eb0256fb3f3af0d
Brad Brandhorst <bradbrandhorst@gmail.com>
2017-10-28 09:39:03 -0400

    carData

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Diagnostic from './components/ODM';
 import YouTubeSeach from './components/YouTube';
+import AddCarData from './components/addCarData';
 
 const App = (props) => 
     <div>
        
         <YouTubeSeach />
         <Diagnostic />
+        <AddCarData />
     </div>
 
 export default App;
