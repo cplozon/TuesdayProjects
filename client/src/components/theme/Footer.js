f4e3c63e1dc4d61975eb5efffc4720dba1a37a9f
cplozon <cory.lozon@yahoo.com>
2017-10-29 10:51:32 -0400

    footerstyleadd

--- client/src/components/theme/Footer.js
+++ client/src/components/theme/Footer.js
@@ -4,10 +4,22 @@ class Footer extends Component {
 	render(){
 		return (
 			<footer id="footer" className="wrapper style1-alt">
+
 				<div className="inner">
-					<ul className="menu">
-						<li>&copy; My Auto Medic | All Rights Reserved</li><li>Design By: Brad Brandhorst | Cory Lozon | Kevin Packler</li>
-					</ul>
+					<div className="row" id="centerfooter">
+          				<div className="col-md-6" id="fineline">
+							<ul className="menu">
+								<li>www.myautomedic.com</li>
+								<li>&copy; My Auto Medic | All Rights Reserved</li>
+							</ul>
+						</div>
+						<div className="col-md-6" id="centerfooter">
+							<ul className="menu">
+								<li>Design By: </li>
+								<li>Brad Brandhorst | Cory Lozon | Kevin Packler</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</footer>
 		)
