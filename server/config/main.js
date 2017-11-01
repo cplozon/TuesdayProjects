a815c1d99172e1cdfa22b6b343105b4d129df14f
johnpmitsch <johnpmitsch@gmail.com>
2017-11-01 18:02:32 -0400

    changed port

--- server/config/main.js
+++ server/config/main.js
@@ -1,13 +1,9 @@
 module.exports = {  
-'port': process.env.PORT || 3000  
-}
-
-module.exports = {  
   // Secret key for JWT signing and encryption
   'secret': 'super secret passphrase',
   // Database connection information
   'database': 'mongodb://localhost/myauto',
   // Setting port for server
-  'port': process.env.PORT || 3000
+  'port': process.env.PORT || 3001
 }
 
