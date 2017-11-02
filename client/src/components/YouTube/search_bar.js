56510a57c077bff04d92ec296ac73e4955ff00da
Brad Brandhorst <bradbrandhorst@gmail.com>
2017-11-01 21:58:47 -0400

    merge bars

--- client/src/components/YouTube/search_bar.js
+++ client/src/components/YouTube/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends React.Component {
         <span className="pure-btn">
           <button className="pure-button" 
           type="button" onClick={(event) => 
-          this.props.onSearchTerm(this.refs.problem.value, this.refs.model.value)}>Search!</button>
+          this.props.onSearchTerm(this.refs.problem.value, this.refs.model.value)}>Search Now!</button>
         </span>
       </div>
     )
